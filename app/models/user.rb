086c53bcc49492d42cd2b5a83e6872dd065b8e24
kinnik <northislandking@gmail.com>
2013-12-09 20:28:50 +0000

    Added tests for user attributes validations; also 'required awesome_print' in the gemfile so that it can be used in testing.

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,15 @@ class User < ActiveRecord::Base
 
   has_many :statuses
 
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :profile_name, presence: true, 
+                           uniqueness: true,
+                           format: { 
+                                with: /\A[a-zA-Z0-9_\-]\z/, 
+                                message: 'Must be formatted correctly.'
+                            }
+
   def full_name
     first_name + " " + last_name
   end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,40 @@
 require 'test_helper'
+require 'awesome_print'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "a user should enter first name" do
+  	user = User.new
+  	assert !user.save
+  	assert !user.errors[:first_name].empty?
+  end
+
+  test "a user should enter last name" do
+  	user = User.new
+  	assert !user.save
+  	assert !user.errors[:last_name].empty?
+  end
+
+  test "a user should enter profile name" do
+  	user = User.new
+  	assert !user.save
+  	assert !user.errors[:profile_name].empty?
+  end 
+
+  test "a user should should have a unique profile name" do
+  	user = User.new
+  	user.profile_name = users(:dummy_test_user_01).profile_name
+
+  	assert !user.save
+  	assert !user.errors[:profile_name].empty?
+  end
+
+  test "a user should have a profile name without spaces" do
+    user = User.new
+    user.profile_name= "My profile with spaces"
+
+    assert !user.save
+    assert !user.errors[:profile_name].empty?
+    assert user.errors[:profile_name].include?("Must be formatted correctly.")
+
+  end
 end
