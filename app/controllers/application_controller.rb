12b3be1b55ae0a234eddc1092a87a95079b0dc11
kinnik <northislandking@gmail.com>
2014-07-28 01:25:26 +0100

    Completion of 'Push Updates' Notes: 1. It is actually JavaScript Polling via AJAX though it is titled 'Push Updates.' 2. There is a known problem that was skipped – when a picture was later deleted, the earlier activity of update is still coming back from the back-end; that would cause the browser to complain because it is not getting the targetable object in the JavaScript code.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,8 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+  rescue_from ActiveRecord::RecordNotFound, with: :render_404
+
   before_filter :configure_permitted_parameters, if: :devise_controller?
 
   private
@@ -14,5 +16,9 @@ class ApplicationController < ActionController::Base
         u.permit(:email, :current_password, :password, :password_confirmation, :first_name, :last_name, :profile_name, :avatar)
     }
   end	
+
+  def render_404
+    render file: 'public/404', status: :not_found
+  end
 
 end
